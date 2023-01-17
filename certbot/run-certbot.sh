38389fce09c28faa3a6a89dd269e317563e4f5be
Kyle Martin <kylemartin87@gmail.com>
2023-01-17 09:45:55 +0200

    Adjust configs to host DataPortal on same server

--- certbot/run-certbot.sh
+++ certbot/run-certbot.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 letsencrypt certonly --webroot -w /var/www/letsencrypt -d "$CN" --agree-tos --email "$EMAIL" --non-interactive --text
+letsencrypt certonly --webroot -w /var/www/letsencrypt -d "$CN2" --agree-tos --email "$EMAIL" --non-interactive --text
 
 cp /etc/letsencrypt/live/"$CN"/cert.pem /var/certs/"$CN"-cert.pem
-cp /etc/letsencrypt/live/"$CN"/chain.pem /var/certs/chain.pem
-cp /etc/letsencrypt/live/"$CN"/fullchain.pem /var/certs/fullchain.pem
+cp /etc/letsencrypt/live/"$CN"/chain.pem /var/certs/"$CN"-chain.pem
+cp /etc/letsencrypt/live/"$CN"/fullchain.pem /var/certs/"$CN"-fullchain.pem
 cp /etc/letsencrypt/live/"$CN"/privkey.pem /var/certs/"$CN"-privkey.pem
 
+cp /etc/letsencrypt/live/"$CN2"/cert.pem /var/certs/"$CN2"-cert.pem
+cp /etc/letsencrypt/live/"$CN2"/chain.pem /var/certs/"$CN2"-chain.pem
+cp /etc/letsencrypt/live/"$CN2"/fullchain.pem /var/certs/"$CN2"-fullchain.pem
+cp /etc/letsencrypt/live/"$CN2"/privkey.pem /var/certs/"$CN2"-privkey.pem
